f45dcf93c266f4e41f333d9828bc0a924adf38fe
theSoberSobber <109434814+theSoberSobber@users.noreply.github.com>
2022-10-06 20:48:11 +0530

    Update getCollegeUpdates.js

--- pages/api/getCollegeUpdates.js
+++ pages/api/getCollegeUpdates.js
@@ -14,6 +14,3 @@ request(options, function (err, res) {
   });
   console.dir(list);
 });
-
-// implement using this, this has a DOM parser
-// https://stackoverflow.com/questions/32655076/cheerio-jquery-selectors-how-to-get-a-list-of-elements-in-nested-divs
